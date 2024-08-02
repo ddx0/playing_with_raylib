34914a560e3fb7b1afe811a0a761a7a5a88a21b2
ddx0 <57961762+ddx0@users.noreply.github.com>
2024-08-01 19:09:42 -0700

    set up simple logging lib

--- main.c
+++ main.c
@@ -1,26 +1,39 @@
 #include "raylib.h"
+#include "./logger/logger.h"
 
 int main(void) {
-    // init
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    // setup logger
+    Logger *logger = NULL;
 
-    InitWindow(screenWidth, screenHeight, "raylib example program");
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        // update variables
-
-        // draw
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("My First Window", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
+    if ((logger = logger_create("./test.txt")) == NULL) {
+        return -1;
     }
 
-    // deinit
-    CloseWindow();
+    logger_sendmsg(logger, "hello");
+    logger_destroy(&logger);
+    // printf("POST_DESTROY: %p\n", (void *) logger);
+
+
+    // // init
+    // const int screenWidth = 800;
+    // const int screenHeight = 450;
+
+    // InitWindow(screenWidth, screenHeight, "raylib example program");
+
+    // SetTargetFPS(60);
+
+    // while (!WindowShouldClose()) {
+    //     // update variables
+
+    //     // draw
+    //     BeginDrawing();
+    //     ClearBackground(RAYWHITE);
+    //     DrawText("My First Window", 190, 200, 20, LIGHTGRAY);
+    //     EndDrawing();
+    // }
+
+    // // deinit
+    // CloseWindow();
 
     return 0;
 }
